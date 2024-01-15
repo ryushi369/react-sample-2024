c48e7f7c007d9f45f2e182a3382c4f8ce011e5d0
ryushi369 <ryushi.hatano@gmail.com>
2024-01-15 16:01:46 +0900

    add: setup eslint, prettier

--- src/components/TextInput.test.tsx
+++ src/components/TextInput.test.tsx
@@ -1,15 +1,15 @@
-import userEvent from "@testing-library/user-event";
-import { render, screen } from "@testing-library/react";
-import TextInput from "./TextInput";
+import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/react';
+import TextInput from './TextInput';
 
-test("TextInput Component Test", async () => {
+test('TextInput Component Test', async () => {
   const user = userEvent.setup();
   render(<TextInput />);
 
-  const textElement = screen.getByText("Entered Text:");
+  const textElement = screen.getByText('Entered Text:');
   expect(textElement).toBeInTheDocument();
 
-  const inputElement = screen.getByLabelText("Text Input");
-  await user.type(inputElement, "Hello World");
-  expect(screen.getByText("Entered Text: Hello World")).toBeInTheDocument();
+  const inputElement = screen.getByLabelText('Text Input');
+  await user.type(inputElement, 'Hello World');
+  expect(screen.getByText('Entered Text: Hello World')).toBeInTheDocument();
 });
